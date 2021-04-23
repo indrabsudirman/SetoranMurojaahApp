3bcf006fd12dce84372609fc325e25411204970b
Indra Sudirman <indrabsudirman@gmail.com>
2021-04-23 13:16:02 +0700

    Add login class and sign up class

--- app/src/main/java/id/indrasudirman/setoranmurojaahapp/MainActivity.java
+++ app/src/main/java/id/indrasudirman/setoranmurojaahapp/MainActivity.java
@@ -1,14 +1,43 @@
 package id.indrasudirman.setoranmurojaahapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+
+import id.indrasudirman.setoranmurojaahapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    //View Binding Variable
+    private ActivityMainBinding mainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        //View Binding change findViewById
+        mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = mainBinding.getRoot();
+        setContentView(view);
+
+        SpannableString spannableString = new SpannableString("Don't have account! Register here!");
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View view) {
+                Intent intent = new Intent(getApplicationContext(), SignUp.class);
+                startActivity(intent);
+                overridePendingTransition(0,0);
+            }
+        };
+        spannableString.setSpan(clickableSpan, 20, 34, 0);
+        mainBinding.appCompatTextViewRegisterLink.setMovementMethod(LinkMovementMethod.getInstance());
+        mainBinding.appCompatTextViewRegisterLink.setText(spannableString);
     }
 }

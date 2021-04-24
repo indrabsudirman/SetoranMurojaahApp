3895b487327381ec6a2d7c859fedac0821abf3d3
Indra Sudirman <indrabsudirman@gmail.com>
2021-04-24 15:29:30 +0700

    Desain on Main Menu

--- app/src/main/java/id/indrasudirman/setoranmurojaahapp/MainMenu.java
+++ app/src/main/java/id/indrasudirman/setoranmurojaahapp/MainMenu.java
@@ -1,8 +1,12 @@
 package id.indrasudirman.setoranmurojaahapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        setToolbar(getString(R.string.app_name));
+    }
+
+    public void setToolbar(@Nullable String title) {
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        CollapsingToolbarLayout collapsingToolbar = findViewById(R.id.collapsingToolbarLayout);
+        collapsingToolbar.setTitle(title);
     }
 }

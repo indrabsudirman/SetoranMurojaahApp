39d6cdeb8bbbf4be6a3f83182a539bb80c118c96
Indra Sudirman <indrabsudirman@gmail.com>
2021-05-04 13:46:53 +0700

    Desain MainMenu using ExtendedFAB

--- app/src/main/java/id/indrasudirman/setoranmurojaahapp/MainMenu.java
+++ app/src/main/java/id/indrasudirman/setoranmurojaahapp/MainMenu.java
@@ -7,10 +7,14 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.widget.TextViewCompat;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.icu.util.IslamicCalendar;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 
@@ -26,14 +30,20 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.Locale;
 
-import id.indrasudirman.setoranmurojaahapp.databinding.ActivityMainBinding;
 import id.indrasudirman.setoranmurojaahapp.databinding.ActivityMainMenuBinding;
 import id.indrasudirman.setoranmurojaahapp.databinding.LayoutToolbarProfileBinding;
+
+
 
 public class MainMenu extends AppCompatActivity {
 
     ActivityMainMenuBinding mainMenuBinding;
     LayoutToolbarProfileBinding layoutToolbarProfileBinding;
+    private SQLiteHelper sqLiteHelper;
+    private SharedPreferences sharedPreferences;
+    private static final String SHARED_PREF_NAME = "sharedPrefLogin";
+    private static final String KEY_EMAIL = "email";
+    private String userEmail;
 
 
     @SuppressLint("SetTextI18n")
@@ -46,6 +56,9 @@ public class MainMenu extends AppCompatActivity {
         mainMenuBinding = ActivityMainMenuBinding.inflate(getLayoutInflater());
         View view = mainMenuBinding.getRoot();
         setContentView(view);
+
+        sharedPreferences = getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
+        userEmail = (sharedPreferences.getString(KEY_EMAIL, "").trim());
 
         layoutToolbarProfileBinding = mainMenuBinding.layoutToolbarProfile;
 
@@ -62,6 +75,19 @@ public class MainMenu extends AppCompatActivity {
         layoutToolbarProfileBinding.tanggalHijriyah.setText(tanggalHijri[1]);
         layoutToolbarProfileBinding.bulanHijriyah.setText(tanggalHijri[0]);
         layoutToolbarProfileBinding.tahunHijriyah.setText(tanggalHijri[2] + " H");
+
+        mainMenuBinding.extendedFab.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), PilihSuratMurojaah.class);
+            startActivity(intent);
+            overridePendingTransition(0,0);
+        });
+
+        mainMenuBinding.logOutAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logOutConfirmation();
+            }
+        });
     }
 
     private String[] setTanggalHijriyah() {
@@ -107,5 +133,31 @@ public class MainMenu extends AppCompatActivity {
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
         CollapsingToolbarLayout collapsingToolbar = findViewById(R.id.collapsingToolbarLayout);
         collapsingToolbar.setTitle(title);
+    }
+
+    private void logOutConfirmation() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainMenu.this);
+        alertDialogBuilder
+                .setTitle("Yakin Keluar!")
+                .setIcon(R.drawable.ic_questions)
+                .setMessage("Anda yakin akan keluar akun ?")
+                .setCancelable(false)
+                .setPositiveButton("Batal keluar",
+                        (dialogInterface, i) -> Toast.makeText(getApplicationContext(), "Anda batal keluar", Toast.LENGTH_SHORT).show())
+
+                .setNegativeButton("Keluar",
+                        (dialogInterface, i) -> {
+                            // Delete SharedPreferences save
+                            sharedPreferences = getSharedPreferences(SHARED_PREF_NAME, 0);
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                            editor.clear();
+                            editor.apply();
+                            startActivity(new Intent(getApplicationContext()
+                                    , MainActivity.class));
+                            overridePendingTransition(0, 0);
+                        });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
     }
 }

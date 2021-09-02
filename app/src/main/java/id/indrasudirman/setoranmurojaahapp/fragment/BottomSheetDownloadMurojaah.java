73362b162a561b2b1fcc3bceaea5b25227f788d0
indrabsudirman <indrabsudirman@gmail.com>
2021-09-02 13:57:19 +0700

    date range picker success and on textview

--- app/src/main/java/id/indrasudirman/setoranmurojaahapp/fragment/BottomSheetDownloadMurojaah.java
+++ app/src/main/java/id/indrasudirman/setoranmurojaahapp/fragment/BottomSheetDownloadMurojaah.java
@@ -1,14 +1,19 @@
 package id.indrasudirman.setoranmurojaahapp.fragment;
 
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.style.UnderlineSpan;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,11 +21,15 @@ import androidx.core.util.Pair;
 
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 import com.google.android.material.snackbar.Snackbar;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import id.indrasudirman.setoranmurojaahapp.R;
@@ -97,17 +106,52 @@ public class BottomSheetDownloadMurojaah extends BottomSheetDialogFragment {
     }
 
     private void pilihTanggalMurojaah() {
+        String[] months = {
+                "Januari", "Februari", "Maret", "April",
+                "Mei", "Juni", "Juli", "Agustus",
+                "September", "Oktober", "November", "Desember"};
+
         MaterialDatePicker.Builder<Pair<Long, Long>> builder = MaterialDatePicker.Builder.dateRangePicker();
         builder.setTitleText("Pilih Tanggal Download");
 //        builder.set
-        MaterialDatePicker materialDatePicker = builder.build();
+        MaterialDatePicker<Pair<Long, Long>> materialDatePicker = builder.build();
 
 
         materialDatePicker.show(getParentFragmentManager(), "TAG");
+        materialDatePicker.addOnPositiveButtonClickListener(selection -> {
+            if (selection.first != null && selection.second != null) {
+                //Start Date
+                Calendar startCalender = Calendar.getInstance();
+                startCalender.setTimeInMillis(selection.first);
+                String startDate1 = startCalender.get(Calendar.DATE) + " " + months[startCalender.get(Calendar.MONTH)] + " " + startCalender.get(Calendar.YEAR);
+                bottomsheetDownloadMurojaahBinding.textViewStartDateShow.setText(startDate1 + " M");
+
+                //End Date
+                Calendar endCalender = Calendar.getInstance();
+                endCalender.setTimeInMillis(selection.second);
+                String endDate1 = endCalender.get(Calendar.DATE) + " " + months[endCalender.get(Calendar.MONTH)] + " " + endCalender.get(Calendar.YEAR);
+                bottomsheetDownloadMurojaahBinding.textViewEndDateShow.setText(endDate1 + " M");
+
+                Log.d("TAG", "OnPositiveButtonClick : " + startDate1 + " "+ endDate1);
+
+            }
+        });
+        //While close by back button, or touch top screen
+        materialDatePicker.addOnCancelListener(dialogInterface ->
+                Snackbar.make(bottomsheetDownloadMurojaahBinding.coordinatorLayoutMain, "Batal memilih tanggal",
+                Snackbar.LENGTH_SHORT).show());
+        //While cancel button was clicked
+        materialDatePicker.addOnNegativeButtonClickListener(view ->
+                Snackbar.make(bottomsheetDownloadMurojaahBinding.coordinatorLayoutMain, "Batal memilih tanggal",
+                        Snackbar.LENGTH_SHORT).show());
+
+
     }
 
     @Override
     public int getTheme() {
         return R.style.BottomSheetDialogTheme;
     }
+
+
 }

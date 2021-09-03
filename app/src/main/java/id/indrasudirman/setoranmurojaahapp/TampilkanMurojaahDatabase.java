7874c1397e9c34c6b2d615b1f5799e8584d42d1b
indrabsudirman <indrabsudirman@gmail.com>
2021-09-03 14:32:38 +0700

    Add Activity Tampil Setoran Murojaah and layout item detail tampil murojaah

--- app/src/main/java/id/indrasudirman/setoranmurojaahapp/TampilkanMurojaahDatabase.java
+++ app/src/main/java/id/indrasudirman/setoranmurojaahapp/TampilkanMurojaahDatabase.java
@@ -2,6 +2,7 @@ package id.indrasudirman.setoranmurojaahapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class TampilkanMurojaahDatabase extends AppCompatActivity {
@@ -10,5 +11,12 @@ public class TampilkanMurojaahDatabase extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tampilkan_murojaah_database);
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(getApplicationContext(), MainMenu.class);
+        startActivity(intent);
+        overridePendingTransition(0, 0);
     }
 }

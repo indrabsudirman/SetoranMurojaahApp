76e314ef16ecd2d09b233e4eb5bd62143e7f5e89
Indra Sudirman <indrabsudirman@gmail.com>
2021-09-29 06:58:31 +0700

    Add method chopped() to chopped or slit arraylist size, to generatedPdf method

--- app/src/main/java/id/indrasudirman/setoranmurojaahapp/fragment/BottomSheetDownloadMurojaah.java
+++ app/src/main/java/id/indrasudirman/setoranmurojaahapp/fragment/BottomSheetDownloadMurojaah.java
@@ -741,6 +741,18 @@ public class BottomSheetDownloadMurojaah extends BottomSheetDialogFragment {
 
     }
 
+    // chops a list into non-view sublists of length L
+    private static <T> List<List<T>> chopped (List<T> list, final int L) {
+        List<List<T>> parts = new ArrayList<List<T>>();
+        final int N = list.size();
+        for (int i = 0; i < N; i+= L) {
+            parts.add(new ArrayList<T>(
+                    list.subList(i, Math.min(N, i + L))
+            ));
+        }
+        return parts;
+    }
+
     @Override
     public int getTheme() {
         return R.style.BottomSheetDialogTheme;
